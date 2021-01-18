3d31777422fbcb49c57411e3a1a2199bee16eb6a
Minseok Suh <strangeloop89@gmail.com>
2021-01-18 13:52:41 +0900

    fix: 토큰 수정

--- src/client.js
+++ src/client.js
@@ -3,14 +3,14 @@ import ApolloClient from "apollo-boost";
 const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
   headers: {
-    Authorization: `bearer 058587e29892a7cae4d6334251f0b735f1d5fbde`,
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_KEY}`,
   },
   service: {
     name: "github",
     url: "https://api.github.com/graphql",
     // optional headers
     headers: {
-      authorization: `bearer 058587e29892a7cae4d6334251f0b735f1d5fbde`,
+      authorization: `bearer ${process.env.REACT_APP_GITHUB_KEY}`,
     },
     // optional disable SSL validation check
     skipSSLValidation: true,
