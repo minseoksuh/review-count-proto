9e3100f8682365c1cf602432ecff82e7261edcb5
Minseok Suh <strangeloop89@gmail.com>
2021-01-18 13:37:39 +0900

    fix: key 처리

--- src/client.js
+++ src/client.js
@@ -3,14 +3,14 @@ import ApolloClient from "apollo-boost";
 const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
   headers: {
-    Authorization: `bearer ${process.env.REACT_APP_GITHUB_KEY}`,
+    Authorization: `bearer 058587e29892a7cae4d6334251f0b735f1d5fbde`,
   },
   service: {
     name: "github",
     url: "https://api.github.com/graphql",
     // optional headers
     headers: {
-      authorization: `bearer ${process.env.REACT_APP_GITHUB_KEY}`,
+      authorization: `bearer 058587e29892a7cae4d6334251f0b735f1d5fbde`,
     },
     // optional disable SSL validation check
     skipSSLValidation: true,
